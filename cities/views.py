3318664da39adaeaaa82905c9eb0ced0b5657895
Ankiwoong <ankiwoong@gmail.com>
2020-06-19 14:28:56 +0900

    Homepage and Search Results Page

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
 
-# Create your views here.
+from .models import City
+
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+
+class SearchResultsView(ListView):
+    model = City
+    template_name = "search_results.html"
+
